2fb3b765d9d53bcc15509f136fcaf369cbfbc40b
retr0-spection <ratinailana@icloud.com>
2024-08-15 19:39:39 +0200

    terraform setup

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-west-2"  # Replace with your preferred region
 }
 
-
 data "aws_security_group" "existing_sg" {
   name = "default"  # Replace with your security group name
 }
@@ -11,27 +10,36 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0a38c1c38a15fed74"  # Amazon Linux 2 AMI
   instance_type = "t2.micro"
 
-  key_name = var.key_name
+  key_name = var.key_name  # Replace with your SSH key name
 
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               sudo yum update -y
-              curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
-              sudo yum install -y nodejs
-              sudo npm install -g pm2
+
+              # Install Docker
+              sudo amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo usermod -a -G docker ec2-user
+              
+              # Clone your application repository
               git clone https://github.com/retr0-spection/campus-transport-server.git /home/ec2-user/app
+
+              # Change to the app directory
               cd /home/ec2-user/app
-              npm install --production
-              pm2 start src/bin/ww.js
+
+              # Build Docker image from Dockerfile
+              sudo docker build -t campus-transport-server .
+
+              # Run the Docker container with auto-restart
+              sudo docker run -d --restart unless-stopped -p 80:3000 campus-transport-server
               EOF
 
   tags = {
-    Name = "ExpressAppServer"
+    Name = "DockerAppServer"
   }
 }
-
 
 output "instance_ip" {
   value = aws_instance.app_server.public_ip
