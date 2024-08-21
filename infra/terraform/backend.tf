cb2147bd4ff99bbeee9c6897572d0a0c3238744c
retr0-spection <ratinailana@icloud.com>
2024-08-21 09:18:07 +0200

    terraform

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "campus-transport"  # Replace with your S3 bucket name
+    bucket         = "campus-transport-bucket"  # Replace with your S3 bucket name
     key            = "terraform/terraform.tfstate"  # The path to the state file within the bucket
     region         = "us-west-2"
     encrypt        = true  # Encrypt the state file
