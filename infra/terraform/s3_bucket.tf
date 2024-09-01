ac4ef3f7c07a9b07620bcc1702e62c1b66be0dc1
retr0-spection <ratinailana@icloud.com>
2024-09-01 18:56:06 +0200

    save

---
+++ infra/terraform/s3_bucket.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "campus-transport"  # Replace with a unique bucket name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name        = "TerraformState"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  hash_key = "LockID"
+}
