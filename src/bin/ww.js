8bf4a0402665ca0e299476de926ff8241ce368cb
retr0-spection <ratinailana@icloud.com>
2024-09-10 21:12:32 +0200

    save

--- src/bin/ww.js
+++ src/bin/ww.js
@@ -1,5 +1,7 @@
 import app from '../app.js'
 import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
 
 //setup mongodb
 const port = 3000
@@ -8,7 +10,7 @@ const dbInit = async () => {
       if (process.env.NODE_ENV == "test") {
         await mongoose.connect()
       } else {
-        await mongoose.connect(process.env.NODE_ENV == "production" ? "mongodb+srv://2327853:VPOPyFASBGnndvjl@cluster0.y8fh3.mongodb.net/campus?retryWrites=true&w=majority&appName=Cluster0" : "mongodb://localhost:27017/campus");
+        await mongoose.connect(process.env.NODE_ENV == "production" ? "mongodb+srv://admin:0FZNTLHqYZHN05iY@cluster0.yral1.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0" : "mongodb://localhost:27017/campus");
       }
       mongoose.set("strictQuery", false);
   
