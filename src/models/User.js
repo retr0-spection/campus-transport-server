30b14934a2b43e6f64992b5d13ebacbf98f306a5
retr0-spection <ratinailana@icloud.com>
2024-10-01 03:55:30 +0200

    save

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import axios from "axios";
 
 // Define the User schema
 const userSchema = new mongoose.Schema({
@@ -35,15 +36,60 @@ userSchema.methods.serializeData = function () {
   return payload;
 };
 
+userSchema.post("save",(doc, next) => {
+    // After saving, check again
+    if (doc.isNew){
+      // call notification API
+      const api = 'http://ec2-52-40-184-137.us-west-2.compute.amazonaws.com/v1/api/notifications/create/notification'
+      const now = new Date();
+      now.setMinutes(now.getMinutes() + 5)
+      const payload = {
+        userId:doc._id,
+        message:"Thanks for joining us, let's get moving!",
+        type:'Welcome',
+        scheduleTime: now
+      }
+
+      console.log("making request")
+
+      const res = axios.post(api, payload)
+
+      
+    }
+    next()
+  });
+
 // Pre-save hook to hash the password before saving
 userSchema.pre("save", async function (next) {
   if (this.isModified("password") || this.isNew) {
-    console.log(this.isModified("password"), this.isNew);
+    try {
+      if (this.isNew){
+        const api = 'http://ec2-52-40-184-137.us-west-2.compute.amazonaws.com/api/v1/notification/create/notification'
+        const now = new Date();
+        now.setMinutes(now.getMinutes() + 5)
+        const payload = {
+          userId:this._id,
+          message:"Thanks for joining us, let's get moving!",
+          type:'Welcome',
+          scheduleTime: now
+        }
+  
+        console.log("making request")
+  
+        const res = axios.post(api, payload)
+      }
+    }catch (err){
+      console.log("could not send welcome notification", err.message)
+    }
+
     try {
       const salt = await bcrypt.genSalt(10);
       this.password = await bcrypt.hash(this.password, salt);
+      
     } catch (error) {
       next(error);
+    }finally{
+      
     }
   } else {
     next();
